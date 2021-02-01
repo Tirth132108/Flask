5323f26ffeadaef8903e4a3c67db38e3d71722a5
Tirth Patel <67674585+Tirth132108@users.noreply.github.com>
2021-02-01 10:28:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import speech_recognition as sr
 
 from werkzeug.datastructures import ContentRange
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -21,45 +23,7 @@ class Todo(db.Model):
 
 @app.route('/', methods =['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content = task_content)
-
-        try:
-                db.session.add(new_task)
-                db.session.commit()
-                return redirect('/')
-
-        except:
-            return "Something Wrong Happen"
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', task = tasks)
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return "There was a problem deleting that task"
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
+    return render_template('index.html')
+    
 if __name__ == "__main__" :
     app.run(debug=True)
